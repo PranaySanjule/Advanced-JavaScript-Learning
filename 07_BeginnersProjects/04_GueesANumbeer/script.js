59c7b573c7152f2ae9355c82f86262e50e80d646
Pranay Sanjule <sanjulepranay0628@gmail.com>
2023-12-07 14:01:25 +0530

    Update Games

--- 07_BeginnersProjects/04_GueesANumbeer/script.js
+++ 07_BeginnersProjects/04_GueesANumbeer/script.js
@@ -1,12 +1,96 @@
-const randomNumber = parseInt(Math.random()*100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
 // or
 // console.log((Math.random()*100 + 1).toFixed(0)); // another way
 
-const submit = document.querySelector('#subt')
-const userInput = document.querySelector('#guessField')
-const guessSlot = document.querySelector('.guesses')
-const remaining = document.querySelector('.lastResult');
-const lowOrHi = document.querySelector('.lowOrHi')
-const startOver = document.querySelector('.resultParas')
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+const startOver = document.querySelector(".resultParas");
 
-c
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  });
+}
+
+// check user input is valid or not (Value in between 1 - 100)
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("Please enter a valid number");
+  } else if (guess < 1) {
+    alert("Please enter a number more than 1");
+  } else if (guess > 100) {
+    alert("Please enter a number less than 100");
+  } else {
+    prevGuess.push(guess);
+    if (numGuess === 11) {
+      displayGuess(guess);
+      displayGuess(`Game Over. Random number was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+
+// check guess is correct, low or high
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayMessage(`${randomNumber} You guessed it RIGHT .`);
+    endGame();
+  } else if (guess < randomNumber) {
+    displayMessage(`Number is TOO Low`);
+  } else if (guess > randomNumber) {
+    displayMessage(`Number is TOO High`);
+  }
+}
+
+// clean input box, update prev guess and remaining guess
+function displayGuess(guess) {
+  userInput.value = "";
+  guessSlot.innerHTML += `${guess}, `;
+  numGuess++;
+  remaining.innerHTML = `${10 - numGuess}`;
+}
+
+// print message value is low or Hi
+function displayMessage(message) {
+  lowOrHi.innerHTML = `<h2>${message}</h2>`;
+}
+
+function endGame() {
+  userInput.value = "";
+  userInput.setAttribute("disabled", "");
+  p.classList.add("button");
+  p.innerHTML = `<h2 id="newGame">Start New Game</h2>`;
+  startOver.appendChild(p);
+  playGame = false;
+  newGame();
+}
+
+function newGame() {
+  const newButtonGame = document.querySelector("#newGame");
+  newButtonGame.addEventListener("click", function (e) {
+    randomNumber = parseInt(Math.random() * 100 + 1);
+    prevGuess = [];
+    numGuess = 1;
+    guessSlot.innerHTML = "";
+    remaining.innerHTML = `${10 - numGuess}`;
+    userInput.removeAttribute("disabled");
+    startOver.removeChild(p);
+    playGame = true;
+  });
+}
